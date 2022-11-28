70efd86995d1072b6fb2b16e46cb2d39c00be736
Mohamed Hamza <manwar@aquilaos.com>
2022-11-28 14:00:52 +0200

    DRY drivers map

--- internal/drivers/drivers.go
+++ internal/drivers/drivers.go
@@ -1,6 +1,7 @@
 package drivers
 
 import (
+    "path"
 	"cuelang.org/go/cue"
 	"devopzilla.com/guku/internal/stack"
 )
@@ -8,4 +9,19 @@ import (
 type Driver interface {
 	match(resource cue.Value) bool
 	ApplyAll(stack *stack.Stack) error
+}
+
+// TODO we need to decompose this into DI pattern
+func NewDriversMap(environment string) map[string]Driver {
+    return map[string]Driver{
+        "compose": &ComposeDriver{
+            Path: path.Join("build", environment, "compose"),
+        },
+        "terraform": &TerraformDriver{
+            Path: path.Join("build", environment, "terraform"),
+        },
+        "kubernetes": &KubernetesDriver{
+            Path: path.Join("build", environment, "kubernetes"),
+        },
+    }
 }
