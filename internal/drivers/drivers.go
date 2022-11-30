1dffb4d93cf1b18352d9b76e02327b4fafcfff9f
George Sedky <george@devopzilla.com>
2022-11-30 11:42:28 +0200

    Add gitlab driver

--- internal/drivers/drivers.go
+++ internal/drivers/drivers.go
@@ -1,7 +1,8 @@
 package drivers
 
 import (
-    "path"
+	"path"
+
 	"cuelang.org/go/cue"
 	"devopzilla.com/guku/internal/stack"
 )
@@ -13,15 +14,18 @@ type Driver interface {
 
 // TODO we need to decompose this into DI pattern
 func NewDriversMap(environment string) map[string]Driver {
-    return map[string]Driver{
-        "compose": &ComposeDriver{
-            Path: path.Join("build", environment, "compose"),
-        },
-        "terraform": &TerraformDriver{
-            Path: path.Join("build", environment, "terraform"),
-        },
-        "kubernetes": &KubernetesDriver{
-            Path: path.Join("build", environment, "kubernetes"),
-        },
-    }
+	return map[string]Driver{
+		"compose": &ComposeDriver{
+			Path: path.Join("build", environment, "compose"),
+		},
+		"terraform": &TerraformDriver{
+			Path: path.Join("build", environment, "terraform"),
+		},
+		"kubernetes": &KubernetesDriver{
+			Path: path.Join("build", environment, "kubernetes"),
+		},
+		"gitlab": &GitlabDriver{
+			Path: path.Join("build", environment, "gitlab"),
+		},
+	}
 }
